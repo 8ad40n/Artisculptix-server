2b1e32368c36826c9baabe80812fc028beee0055
8ad40n <joy184110@gmail.com>
2024-08-02 02:27:24 +0600

    Connect

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // await client.connect();
+    await client.connect();
 
     const userCollection = client.db("artCraftDB").collection("users");
     const craftCollection = client.db("artCraftDB").collection("craft");
