284ba293b65add804946f6e9f1e069b765343560
8ad40n <joy184110@gmail.com>
2024-08-02 02:42:39 +0600

    json updated

--- index.js
+++ index.js
@@ -7,15 +7,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 // Middleware
-// app.use(cors());
-const corsConfig = {
-  origin: '',
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE']
-}
-app.use(cors(corsConfig))
-app.options("", cors(corsConfig))
-
+app.use(cors());
 app.use(express.json());
 
 
